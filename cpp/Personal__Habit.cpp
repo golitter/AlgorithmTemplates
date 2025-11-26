3cbf9ee9d0d51dff95695e9694467b6a5da9af6c
golitter <1981971524@qq.com>
2025-11-26 19:46:13 +0800

    update:个人模型添加auto-dfs形式的代码用法

--- cpp/Personal__Habit.cpp
+++ cpp/Personal__Habit.cpp
@@ -8,7 +8,6 @@
  * 
  * 对拍
  * https://blog.csdn.net/weixin_50624971/article/details/121185941
- * fast_OI 超级快读
 */
 #include <iostream>
 #include <vector>
@@ -79,51 +78,6 @@ namespace d4 {
 vector<int> fx({0,0,1,-1}), fy({1,-1,0,0});
 }
 }
-namespace fast_IO {
-    inline char read() {
-        return getchar();
-        static const int IN_LEN = 1000000;
-        static char buf[IN_LEN], *s, *t;
-        if (s == t) {
-            t = (s = buf) + fread(buf, 1, IN_LEN, stdin);
-            if (s == t) return -1;
-        }
-        return *s++;
-    }
-    template<class T>
-    inline void read(T &x) {
-        static bool iosig;
-        static char c;
-        for (iosig = false, c = read(); !isdigit(c); c = read()) {
-            if (c == '-') iosig = true;
-            if (c == -1) return;
-        }
-        for (x = 0; isdigit(c); c = read())
-            x = ((x + (x << 2)) << 1) + (c ^ '0');
-        if (iosig) x = -x;
-    }
-    const int OUT_LEN = 10000000;
-    char obuf[OUT_LEN], *ooh = obuf;
-    inline void print(char c) {
-        if (ooh == obuf + OUT_LEN) fwrite(obuf, 1, OUT_LEN, stdout), ooh = obuf;
-        *ooh++ = c;
-    }
-    template<class T>
-    inline void print(T x) {
-        static int buf[30], cnt;
-        if (x == 0) {
-            print('0');
-        }
-        else {
-            if (x < 0) print('-'), x = -x;
-            for (cnt = 0; x; x /= 10) buf[++cnt] = x % 10 + 48;
-            while (cnt) print((char)buf[cnt--]);
-        }
-    }
-    inline void flush() {
-        fwrite(obuf, 1, ooh - obuf, stdout);
-    }
-}
 void numeric() { // 数学
     LL k = 1e17;
     // cmath库里的数学尽量先乘个 1.0
@@ -151,6 +105,11 @@ void solve() {
     auto lam = [&](int a, int b) -> int {
         return a > b ? a : b;
     }; // 注意 逗号
+
+    // 递归形式
+    auto dfs = [&](auto &&self, int u) -> void {
+        ;
+    };
 }
 int main() // signed main()
 {
